11514f33e8a3cd7e320321f9dbaeef26cc7c0211
Matt Gilg <gilgm12@gmail.com>
2015-12-08 23:28:21 -0700

    Line ending adjustments.

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
-#/bin/sh
+#!/bin/sh
 
 APPNAME=${1?"Please specify an application name and try again."};
 
 # Fetch Bootstrap Files.
 cd Bootstrap;
-BootstrapFiles=$(find . -type f)
 
-for file in $BootstrapFiles;do
+BootstrapFiles=$(find . -not -path '*/\.*' -type f)
+
+for file in $BootstrapFiles;
+do
+	echo "Processing " + $file;
+
+	# Convert line endings
+	sed -i '' -e 's/\r\+$//' $file;
+
 	# Replace file contents
-	sed -i "s/Bootstrap/$APPNAME/g" $file;
+	sed -i '' -e "s/Bootstrap/$APPNAME/g" $file;
 
 	# Fetch updated filename.
 	newfile=$(echo $file | sed s/Bootstrap/$APPNAME/g)
